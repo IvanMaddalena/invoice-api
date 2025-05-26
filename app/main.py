55e5feb943658da3ee1cbb7d6d31b935db6bafa2
Ivan Maddalena <73302759+IvanMaddalena@users.noreply.github.com>
2025-05-26 15:26:30 +0200

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.routes import router
+from app.database import create_db_and_tables
+
+app = FastAPI(title="Invoice API")
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+app.include_router(router)
