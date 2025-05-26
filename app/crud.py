f5cc3451918f79aa2edd88e4b1fbf81ed4790151
Ivan Maddalena <73302759+IvanMaddalena@users.noreply.github.com>
2025-05-26 15:24:45 +0200

    Create crud.py

---
+++ app/crud.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+from app import models, schemas
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(name=user.name, email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_users(db: Session):
+    return db.query(models.User).all()
+
+def create_invoice(db: Session, invoice: schemas.InvoiceCreate):
+    db_invoice = models.Invoice(**invoice.dict())
+    db.add(db_invoice)
+    db.commit()
+    db.refresh(db_invoice)
+    return db_invoice
+
+def get_invoices(db: Session):
+    return db.query(models.Invoice).all()
